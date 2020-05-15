b14a329b8d05591cd7bbdcc655f9df32ae48cb87
DJ Zevenbergen <djzevenbergen@gmail.com>
2020-05-15 16:06:14 -0600

    add loop for program and an end program method

--- WordCounter/Program.cs
+++ WordCounter/Program.cs
@@ -14,48 +14,59 @@ namespace WordCounter
 
       while (!view)
       {
+        view = RunProgramLoop();
+      }
+
+      if (view == true)
+      {
+        EndProgram();
+      }
+    }
+
+    public static void EndProgram()
+    {
+      foreach (RepeatCounter i in RepeatCounter.GetInstances())
+      {
+        Console.WriteLine($"sentence, {i.Sentence}, contained, {i.Word}, count, {i.GetCount()}");
+      }
+      Console.ReadLine();
+      Environment.Exit(0);
+    }
+    public static bool RunProgramLoop()
+    {
+      bool view = false;
+      string[] inputs = ReturnValidStrings();
+
+      RepeatCounter newCounter = new RepeatCounter(inputs[0], inputs[1]);
+      string t = "";
+
+      if (newCounter.GetCount() > 1)
+      {
+        t = "times";
+      }
+      else
+      {
+        t = "time";
+      }
+      Console.WriteLine($"The word '{newCounter.Word}' appears {newCounter.GetCount()} {t} in the sentence '{newCounter.Sentence}' ");
 
 
-        string[] inputs = ReturnValidStrings();
-
-        RepeatCounter newCounter = new RepeatCounter(inputs[0], inputs[1]);
-        string t = "";
-
-        if (newCounter.GetCount() > 1)
+      if (RepeatCounter.GetInstances().Count > 0)
+      {
+        Console.WriteLine("If you want to try again, press Enter.");
+        Console.WriteLine("If you want to [V]iew your previous entries, press V and enter.");
+        string resp = Console.ReadLine().ToLower();
+        if (resp == "v")
         {
-          t = "times";
+          view = true;
         }
         else
         {
-          t = "time";
-        }
-        Console.WriteLine($"The word '{newCounter.Word}' appears {newCounter.GetCount()} {t} in the sentence '{newCounter.Sentence}' ");
-
-
-        if (RepeatCounter.GetInstances().Count > 0)
-        {
-          Console.WriteLine("If you want to try again, press Enter.")
-          Console.WriteLine("If you want to [V]iew your previous entries, press V and enter.");
-          string resp = Console.ReadLine().ToLower();
-          if (resp == "v")
-          {
-            view = true;
-          }
-          else
-          {
-            view = false;
-          }
-        }
-        if (view == true)
-        {
-          foreach (RepeatCounter i in RepeatCounter.GetInstances())
-          {
-            Console.WriteLine($"{i.Sentence} contained {i.Word} {i.GetCount()} times");
-          }
-          Console.ReadLine();
-          Environment.Exit(0);
+          view = false;
         }
       }
+
+      return view;
     }
 
     public static string[] ReturnValidStrings()
@@ -87,7 +98,6 @@ namespace WordCounter
       }
       validInputs[0] = sentResp;
       validInputs[1] = wordResp;
-
       return validInputs;
 
     }
