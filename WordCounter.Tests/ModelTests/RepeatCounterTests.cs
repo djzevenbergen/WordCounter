9f965c7713c87a06a315299c57d2002948287973
DJ Zevenbergen <djzevenbergen@gmail.com>
2020-05-15 10:06:48 -0600

    add constructor tests

--- WordCounter.Tests/ModelTests/RepeatCounterTests.cs
+++ WordCounter.Tests/ModelTests/RepeatCounterTests.cs
@@ -9,6 +9,27 @@ namespace WordCounter.Tests
   {
     [TestMethod]
 
+    public void RepeatCounter_IsInitialized_RepeatCounter()
+    {
+      string sent = "There is a rat over there";
+      string word = "there";
+      RepeatCounter testCounter = new RepeatCounter(sent, word);
+
+      Assert.AreEqual(typeof(RepeatCounter), testCounter.GetType());
+    }
+
+    [TestMethod]
+
+    public void RepeatCounter_PropertiesAreSet_StringStringInt()
+    {
+      string sent = "There is a rat over there";
+      string word = "there";
+      RepeatCounter testCounter = new RepeatCounter(sent, word);
+
+      Assert.AreEqual(sent, testCounter.Sentence);
+      Assert.AreEqual(word, testCounter.Word);
+      Assert.AreEqual(2, testCounter.GetCount());
+    }
 
 
   }
